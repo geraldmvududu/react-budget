c0ee8262a017a568abaee747edc6261c8e8c9892
geraldmvududu <86417536+geraldmvududu@users.noreply.github.com>
2023-03-22 18:41:42 +0200

    first commit

---
+++ src/actions/logout.js
@@ -0,0 +1,18 @@
+//rrd import
+import { redirect } from "react-router-dom";
+
+//Library
+import { toast } from "react-toastify";
+
+//helpers
+import { deleteItem } from "../helpers";
+
+export async function logoutAction() {
+  //delete the user
+  deleteItem({ key: "userName" });
+  deleteItem({ key: "budgets" });
+  deleteItem({ key: "expenses" });
+  toast.success("You've deleted your account");
+  //return redirect
+  return redirect("/");
+}
